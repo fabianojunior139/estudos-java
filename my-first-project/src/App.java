79af804c9e25379700ac8075eb592ffd349e1465
Fabiano Júnior <fabianojunior139@gmail.com>
2023-01-24 03:46:14 -0300

    finishing the first exercises list

--- my-first-project/src/App.java
+++ my-first-project/src/App.java
@@ -3,7 +3,7 @@ import exercises.*;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        exercise_28 lesson = new exercise_28();
+        exercise_35 lesson = new exercise_35();
         lesson.execute();
     }
 }
---
+++ my-first-project/src/exercises/exercise_34.java
@@ -0,0 +1,36 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class exercise_34 {
+    public void execute() {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Write the swimmer's age: ");
+        int age = scan.nextInt();
+
+        if (age >= 5 && age <= 25) {
+            if (age >= 5 && age <= 7) {
+                System.out.println("Category: A-Childish");
+
+            } else if (age >= 8 && age <= 10) {
+                System.out.println("Category: B-Childish");
+
+            } else if (age >= 11 && age <= 13) {
+                System.out.println("Category: A-Juvenile");
+
+            } else if (age >= 14 && age <= 17) {
+                System.out.println("Category: B-Juvenile");
+
+            } else if (age >= 18 && age <= 25) {
+                System.out.println("Category: Senior");
+            }
+        } else {
+            System.out.println("Out-of-age swimmer");
+        }
+
+        System.out.println("");
+        scan.close();
+    }
+}
